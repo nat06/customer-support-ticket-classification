a91a9fecefa7563a1a80d346971df4c029ca5625
nathanpollet <nathan.pollet2@gmail.com>
2025-07-11 16:54:04 +0200

    Initial commit

---
+++ utils.py
@@ -0,0 +1,17 @@
+import os
+import re
+import json
+import pandas as pd
+
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"[^a-zA-Z0-9.,!?;:'\"()\[\]{}@/\-\s]", "", text)
+    text = re.sub(r"\s+", " ", text).strip()
+    return text
+
+
+def normalize_prediction(pred, queue_list):
+    pred = pred.strip().lower()
+    normalized = next((q for q in queue_list if q.lower() in pred), "UNKNOWN")
+    return normalized
